e08d2f7287ccc44345d5c79a6037a449444bdd24
zulkarnaenfhr <fahrizulkarnaen1@gmail.com>
2026-08-02 12:57:29 +0700

    add gzipper for building project

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,8 @@ export default defineConfig({
 	plugins: [react()],
 	base: '/',
 });
+git status
+git add .
+git commit -m "add gzipper for building project"
+npm run deploy
+git push -u origin main
